fe41fc2adfb03e4811e0a66ade82c4125031e553
timf34 <farrelti@tcd.ie>
2023-04-07 16:48:13 +0100

    checkpoint before I try the date tiem picker example directly from the repo

--- App.js
+++ App.js
@@ -1,34 +1,39 @@
-import React from 'react';
-import { StyleSheet, TouchableOpacity, Text, View } from 'react-native';
-import { playAlarm } from './AlarmPlayer';
+import React, { useState } from 'react';
+import { StyleSheet, TouchableOpacity, Text, View, Alert } from 'react-native';
+import DateTimePickerModal from 'react-native-modal-datetime-picker';
+import { format } from 'date-fns';
 
 function App() {
+  const [alarmTime, setAlarmTime] = useState(null);
+  const [isDateTimePickerVisible, setIsDateTimePickerVisible] = useState(false);
+
+  const showDateTimePicker = () => setIsDateTimePickerVisible(true);
+
+  const hideDateTimePicker = () => setIsDateTimePickerVisible(false);
+
+  const handleDatePicked = (date) => {
+    setAlarmTime(date);
+    hideDateTimePicker();
+    setTimeout(() => {
+      playAlarm();
+      Alert.alert('Alarm', 'Wake up!', [{ text: 'Stop', onPress: stopAlarm }]);
+    }, date.getTime() - Date.now());
+  };
+
   return (
     <View style={styles.container}>
-      <TouchableOpacity style={styles.button} onPress={playAlarm}>
-        <Text style={styles.buttonText}>Play Alarm</Text>
+      <TouchableOpacity style={styles.button} onPress={showDateTimePicker}>
+        <Text style={styles.buttonText}>Set Alarm</Text>
       </TouchableOpacity>
+      {alarmTime && (
+        <Text style={styles.alarmText}>Alarm set for {format(alarmTime, 'h:mm a')}</Text>
+      )}
+      <DateTimePickerModal
+        isVisible={isDateTimePickerVisible}
+        mode="time"
+        onConfirm={handleDatePicked}
+        onCancel={hideDateTimePicker}
+      />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  button: {
-    backgroundColor: '#007AFF',
-    padding: 10,
-    borderRadius: 5,
-  },
-  buttonText: {
-    color: '#fff',
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-});
-
-export default App;
