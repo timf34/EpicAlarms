babe3f20607b2bca9f5f2a1a5ea046360ebce3ee
timf34 <farrelti@tcd.ie>
2023-04-07 16:58:08 +0100

    Time picker button working; its not connected to the music player, and there's no way to turn it off currently

--- App.js
+++ App.js
@@ -1,39 +1,70 @@
-import React, { useState } from 'react';
-import { StyleSheet, TouchableOpacity, Text, View, Alert } from 'react-native';
-import DateTimePickerModal from 'react-native-modal-datetime-picker';
-import { format } from 'date-fns';
+import React, { useState } from "react";
+import { Button, Platform, StyleSheet, Switch, Text, View } from "react-native";
+import DateTimePickerModal from "react-native-modal-datetime-picker";
 
-function App() {
-  const [alarmTime, setAlarmTime] = useState(null);
-  const [isDateTimePickerVisible, setIsDateTimePickerVisible] = useState(false);
+const App = () => {
+  const [pickerMode, setPickerMode] = useState(null);
+  const [inline, setInline] = useState(false);
 
-  const showDateTimePicker = () => setIsDateTimePickerVisible(true);
+  const showDatePicker = () => {
+    setPickerMode("date");
+  };
 
-  const hideDateTimePicker = () => setIsDateTimePickerVisible(false);
+  const showTimePicker = () => {
+    setPickerMode("time");
+  };
 
-  const handleDatePicked = (date) => {
-    setAlarmTime(date);
-    hideDateTimePicker();
-    setTimeout(() => {
-      playAlarm();
-      Alert.alert('Alarm', 'Wake up!', [{ text: 'Stop', onPress: stopAlarm }]);
-    }, date.getTime() - Date.now());
+  const showDateTimePicker = () => {
+    setPickerMode("datetime");
+  };
+
+  const hidePicker = () => {
+    setPickerMode(null);
+  };
+
+  const handleConfirm = (date) => {
+    // In order to prevent the double-shown popup bug on Android, picker has to be hidden first (https://github.com/react-native-datetimepicker/datetimepicker/issues/54#issuecomment-618776550)
+    hidePicker();
+    console.warn("A date has been picked: ", date);
   };
 
   return (
-    <View style={styles.container}>
-      <TouchableOpacity style={styles.button} onPress={showDateTimePicker}>
-        <Text style={styles.buttonText}>Set Alarm</Text>
-      </TouchableOpacity>
-      {alarmTime && (
-        <Text style={styles.alarmText}>Alarm set for {format(alarmTime, 'h:mm a')}</Text>
+    <View style={style.root}>
+      <Button title="Show Date Picker" onPress={showDatePicker} />
+      <Button title="Show Time Picker" onPress={showTimePicker} />
+      <Button title="Show DateTime Picker" onPress={showDateTimePicker} />
+      {Platform.OS === "ios" && (
+        <View style={style.inlineSwitchContainer}>
+          <Text style={style.inlineSwitchText}>Display inline?</Text>
+          <Switch value={inline} onValueChange={setInline} />
+        </View>
       )}
       <DateTimePickerModal
-        isVisible={isDateTimePickerVisible}
-        mode="time"
-        onConfirm={handleDatePicked}
-        onCancel={hideDateTimePicker}
+        isVisible={pickerMode !== null}
+        mode={pickerMode}
+        onConfirm={handleConfirm}
+        onCancel={hidePicker}
+        display={inline ? "inline" : undefined}
       />
     </View>
   );
-}
+};
+
+const style = StyleSheet.create({
+  root: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  inlineSwitchContainer: {
+    marginTop: 28,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  inlineSwitchText: {
+    fontSize: 18,
+    marginRight: 8,
+  },
+});
+
+export default App;
